3683352235903f2ecb8652bbe8c06323c3aa2bfe
Tarun Sharma <kunal1381995@gmail.com>
2018-09-10 23:02:43 +0530

    dropDownButton & dropDownView added.

--- dropDownMenu/ViewController.swift
+++ dropDownMenu/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var button = dropDownButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        button = dropDownButton.init(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        button.setTitle("Colors", for: .normal)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(button)
+        
+        button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        
+        button.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +38,67 @@ class ViewController: UIViewController {
 
 
 }
+
+class dropDownButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = .cyan
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+class dropDownView : UIView, UITableViewDelegate, UITableViewDataSource {
+    
+    var dropDownOptions = [String]()
+    var tableView =  UITableView()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubview(tableView)
+        
+        tableView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.dropDownOptions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = dropDownOptions[indexPath.row]
+        return cell
+    }
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
 
